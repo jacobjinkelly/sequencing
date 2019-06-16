598e40c6c6ff70615f4f45b90447a14eb1c4bc20
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-06-16 17:32:40 -0400

    add description to fasta

--- format_test.sh
+++ format_test.sh
@@ -7,8 +7,8 @@ do
     echo $i
     name=$(echo $i | cut -f 1 -d '.')
     mkdir ${name} &> /dev/null
-    echo ">" > ${name}/p.fasta
+    echo \>${name} > ${name}/p.fasta
     echo $(sed '1!d' $i) >> ${name}/p.fasta
-    echo ">" > ${name}/t.fasta
+    echo \>${name} > ${name}/t.fasta
     echo $(sed '2!d' $i) >> ${name}/t.fasta
 done
