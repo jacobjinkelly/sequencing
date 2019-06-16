97b737d5d8f0470b4709f00ea27f8fe93bed82c5
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-06-16 17:48:14 -0400

    consistently use fa format

--- format_test.sh
+++ format_test.sh
@@ -7,8 +7,8 @@ do
     echo $i
     name=$(echo $i | cut -f 1 -d '.')
     mkdir ${name} &> /dev/null
-    echo \>${name} > ${name}/p.fasta
-    echo $(sed '1!d' $i) >> ${name}/p.fasta
-    echo \>${name} > ${name}/t.fasta
-    echo $(sed '2!d' $i) >> ${name}/t.fasta
+    echo \>${name} > ${name}/p.fa
+    echo $(sed '1!d' $i) >> ${name}/p.fa
+    echo \>${name} > ${name}/t.fa
+    echo $(sed '2!d' $i) >> ${name}/t.fa
 done
