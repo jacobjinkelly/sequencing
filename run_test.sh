26e22114f797ce66f18e8a5880b444d3a0512dfe
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-06-16 17:32:51 -0400

    reformat test to fasta

--- run_test.sh
+++ run_test.sh
@@ -6,10 +6,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-for i in tests/*.in
+for i in tests/*/
 do
     echo $i
-    ./align.exe $(<$i)
+    ./align.exe ${i}p.fasta ${i}t.fasta
 done
 
 make clean
